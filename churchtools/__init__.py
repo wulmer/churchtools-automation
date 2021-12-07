8390603373e9b51a08fe2216b73d1e5ea31b7c8c
Wolfgang Ulmer <wulmer@web.de>
2021-12-07 23:37:11 +0100

    Move class to own module

---
+++ churchtools/__init__.py
@@ -0,0 +1,60 @@
+import requests
+
+
+class ChurchToolsApi:
+    def __init__(self, base_url, token):
+        self._base_url = base_url
+        self._token = token
+        self._session = requests.Session()
+        self._session.headers.update({"Authorization": f"Login {token}"})
+
+    def add_to_group(self, who, to):
+        response = self._session.put(self._base_url + f"/groups/{to}/members/{who}")
+        response.raise_for_status()
+
+    def remove_from_group(self, who, from_):
+        response = self._session.delete(
+            self._base_url + f"/groups/{from_}/members/{who}"
+        )
+        response.raise_for_status()
+
+    def get_persons(self, statuses=None):
+        params = {}
+        if statuses is not None:
+            params["status_ids[]"] = statuses
+        for person in self.paginate(self._base_url + "/persons", params=params):
+            yield person
+
+    def get_groups(self, query=None):
+        params = {}
+        if query is not None:
+            params["query"] = query
+        for group in self.paginate(self._base_url + "/groups", params=params):
+            yield group
+
+    def get_group_members(self, group_id):
+        for member in self.paginate(self._base_url + f"/groups/{group_id}/members"):
+            yield member
+
+    def get_statuses(self):
+        for status in self.paginate(self._base_url + "/statuses"):
+            yield status
+
+    def paginate(self, url, params=None):
+        if params is None:
+            params = {}
+        last_page = 999
+        current_page = 1
+        while current_page <= last_page:
+            params.update({"page": current_page})
+            response = self._session.get(url, params=params)
+            response.raise_for_status()
+            paged_data = response.json()
+            for data in paged_data["data"]:
+                yield data
+            meta = paged_data["meta"]
+            if "pagination" in meta:
+                current_page = meta["pagination"]["current"] + 1
+                last_page = meta["pagination"]["lastPage"]
+            else:
+                break

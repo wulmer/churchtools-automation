80ddcbba19ca43ca1852635a1c58f7c0f969c89a
Wolfgang Ulmer <wulmer@web.de>
2022-01-05 17:12:22 +0100

    Send HTML mails

--- checkGodiPlan.py
+++ checkGodiPlan.py
@@ -1,17 +1,52 @@
 import os
 import smtplib
-from email.message import EmailMessage
+import subprocess
+import sys
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from gottesdienstplan import GoDiPlanChecker
 
+MAIL_TEXT_TEMPLATE = """
+Hallo,
+
+im Gottesdienstplan scheint es einen Fehler, bzw. einen fehlenden Eintrag zu geben.
+Bitte ergänze die fehlende Information oder korrigiere den falschen Eintrag.
+
+Um was es sich genau handelt:
+
+{message}
+
+Liebe Grüße
+Dein freundlicher Gottesdienstplan Checker
+"""
+
+MAIL_HTML_TEMPLATE = """
+<p>Hallo,</p>
+<p>
+im Gottesdienstplan scheint es einen Fehler, bzw. einen fehlenden Eintrag zu geben.
+Bitte ergänze die fehlende Information oder korrigiere den falschen Eintrag.
+</p>
+<p>
+Um was es sich genau handelt:
+</p>
+<p><em>
+{message}
+</em></p>
+<p>
+<p>Liebe Grüße</p>
+<p>Dein freundlicher Gottesdienstplan Checker</p>
+"""
+
 
 class Mailer:
-    def __init__(self, my_addr):
+    def __init__(self, my_addr, port: int = 25):
         self._server = None
         self._my_addr = my_addr
+        self._port = port
 
     def __enter__(self):
-        self._server = smtplib.SMTP("localhost")
+        self._server = smtplib.SMTP("localhost", port=self._port)
         return self
 
     def __exit__(self, type, value, traceback):
@@ -22,11 +57,12 @@ class Mailer:
         msg = data["message"]
         # FIXME: recp = data["recipient"]
         recp = f"webmaster@{MAIL_DOMAIN}"
-        mail = EmailMessage()
-        mail.set_content(msg)
+        mail = MIMEMultipart("alternative")
         mail["Subject"] = "Gottesdienstplan Checker Nachricht"
         mail["From"] = self._my_addr
         mail["To"] = recp
+        mail.attach(MIMEText(MAIL_TEXT_TEMPLATE.format(message=msg), "plain"))
+        mail.attach(MIMEText(MAIL_HTML_TEMPLATE.format(message=msg), "html"))
         self._server.send_message(
             to_addrs=recp,
             from_addr=self._my_addr,
@@ -36,10 +72,32 @@ class Mailer:
 
 if __name__ == "__main__":
     MAIL_DOMAIN = os.environ.get("MAIL_DOMAIN")
-    if MAIL_DOMAIN:
-        p = GoDiPlanChecker(mail_domain=MAIL_DOMAIN)
-        with Mailer(my_addr=f"godiplanchecker@{MAIL_DOMAIN}") as m:
-            p.check("4w", reporter=m.handle_check_report)
-    else:
-        p = GoDiPlanChecker()
-        p.check("4w", reporter=print)
+    port = 25
+    smtpd_process = None
+
+    if not MAIL_DOMAIN:
+        port = 1025
+        smtpd_process = subprocess.Popen(
+            [
+                sys.executable,
+                "-m",
+                "smtpd",
+                "-c",
+                "DebuggingServer",
+                "-n",
+                f"localhost:{port}",
+            ]
+        )
+
+    p = GoDiPlanChecker(mail_domain=MAIL_DOMAIN)
+    with Mailer(my_addr=f"godiplanchecker@{MAIL_DOMAIN}", port=port) as m:
+        p.check("4w", reporter=m.handle_check_report)
+
+    if smtpd_process is not None:
+        try:
+            outs, errs = smtpd_process.communicate(timeout=1)
+        except subprocess.TimeoutExpired:
+            smtpd_process.kill()
+            outs, errs = smtpd_process.communicate()
+        print(outs)
+        print(errs)

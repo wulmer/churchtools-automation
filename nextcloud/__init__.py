62a5eaa5d578720c2016dd5aab78ba49fd28b71b
Wolfgang Ulmer <wolfgang.ulmer@de.bosch.com>
2026-05-21 22:27:32 +0200

    feat: Retry for NextCloud operations

--- nextcloud/__init__.py
+++ nextcloud/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import httpx
+import tenacity
 from webdav4.fsspec import WebdavFileSystem
 
 
@@ -7,12 +9,30 @@ class NextCloud:
     def __init__(self, webdav_url: str, webdav_auth):
         self._fs = WebdavFileSystem(webdav_url, auth=webdav_auth)
 
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(5),
+        wait=tenacity.wait_exponential_jitter(initial=1, max=60, jitter=2),
+        retry=tenacity.retry_if_exception_type(httpx.ReadTimeout),
+        reraise=True,
+    )
     def ls(self, path: str = "/", detail: bool = False):
         return self._fs.ls(path, detail=detail)
 
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(5),
+        wait=tenacity.wait_exponential_jitter(initial=1, max=60, jitter=2),
+        retry=tenacity.retry_if_exception_type(httpx.ReadTimeout),
+        reraise=True,
+    )
     def exists(self, path: str):
         return self._fs.exists(path)
 
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(5),
+        wait=tenacity.wait_exponential_jitter(initial=1, max=60, jitter=2),
+        retry=tenacity.retry_if_exception_type(httpx.ReadTimeout),
+        reraise=True,
+    )
     def mkdir(self, path: str):
         return self._fs.makedirs(path, exist_ok=True)
 
